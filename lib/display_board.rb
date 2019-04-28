06fe211ed7aa7b7e2801a36578ed7c6f20ac8d57
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-04-28 20:18:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,5 +3,9 @@
 board = Array.new
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 def display_board(board)
-  puts display_board
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
