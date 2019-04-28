775d3182bb0f6045e071cc32bf42b5236eca1397
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-04-28 20:06:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = Array.new
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+def display_board(board)
+  puts display_board
+end
